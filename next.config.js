fab87036512162364a6c16838539205da32efe9a
Mahdi Darabi <mahdidarabi18@gmail.com>
2026-08-31 13:45:40 +0330

    chore: add md7.ir domain support

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const isGitHubPagesBuild = process.env.GITHUB_ACTIONS === "true";
-const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1];
-const basePath =
-  isGitHubPagesBuild && repositoryName ? `/${repositoryName}` : undefined;
 
 const nextConfig = {
   output: isGitHubPagesBuild ? "export" : "standalone",
-  basePath,
-  assetPrefix: basePath ? `${basePath}/` : undefined,
   trailingSlash: true,
 };
 
